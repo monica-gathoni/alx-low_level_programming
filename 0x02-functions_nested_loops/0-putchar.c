05537bbd2c742aa0ce91805682e9d2c71b958512
monica-gathoni <monicmaish201@gmail.com>
2023-06-21 03:29:36 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * main - prints _putchar and a new line
- *   * Return: Always 0
- *    */
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
 int main(void)
 {
 		_putchar('_');
